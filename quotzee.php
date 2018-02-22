861452fb90ea1917144b40d7abcbcbede12b9e96
Farooq khan <vsfarooqkhan@gmail.com>
2018-02-22 12:36:48 +0530

    Create quotzee.php

---
+++ quotzee.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+<html>
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body bgcolor="#d0d2e2">
+
+<!-- Title for quotzee -->
+<h1 style = "color:#68010b"> QUoTZee</h1>
+
+<!--Form for login and register  -->
+<form >
+	<div class="container">
+	<br/>	
+
+	<button type="submit" style="height:50px;weight:150px;" onclick="location.href='login.php';" value="Login" class="btn btn-primary">Login</button>
+
+
+
+</form>
+
+<p>
+<!-- Link for Registration -->
+<a href = " register.php"> Not registered yet?? Click here to register. </a>
+</p>
+
+
+<img src="https://www.brainyquote.com/photos_tr/en/s/samlevenson/166055/samlevenson1-2x.jpg" alt="quote"height ="300" weight="500">
+
+<body>
+</html>
