6e4a44d2525b8f379594349ba97dc01545497840
Farooq khan <vsfarooqkhan@gmail.com>
2018-02-22 12:41:06 +0530

    Create post.php

---
+++ post.php
@@ -0,0 +1,24 @@
+<?php
+?>
+
+<html>
+	<body>
+		<form method="POST" action="quote.php">
+		
+name:<br/>
+<input type="text" name="name"> 
+		<br />
+		Title: <br />
+		<input type="text" name="title">
+<br/>
+Post your quote: <br>
+		<textarea rows="8" cols="80" name="post"  id="post">
+		</textarea>
+
+
+			<input type="submit" name="submit">
+
+
+		</form>
+	</body>
+</html>
