14a647a10c445154c769d3743dfc98f153d1ff43
Farooq khan <vsfarooqkhan@gmail.com>
2018-02-22 12:37:37 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+<html>
+<body bgcolor="#04e1f9">
+
+
+<h1 style="color:#720b3d;font-family:chiller"> QUoTZee </h1>
+
+<form action="loggin.php" action="homepage.php" name="login" method="post">
+
+User Name:<input type="text" name="name"/><br />
+
+Password &nbsp; :<input type="password" name="passwd"/><br />
+
+
+<input type="submit" name="submit">
+<h4 style="color:#10198c"> Not a user yet??  <a href="register.php" > Click here to register </a>  </h4>
+
+</form>
+
+</body>
+</html>
