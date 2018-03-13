882af63d48da3d4e6037e8294a432c56c27e9ce6
Farooq khan <vsfarooqkhan@gmail.com>
2018-03-13 19:37:01 +0530

    Update homepage.php
    updated with bootstrap

--- homepage.php
+++ homepage.php
@@ -1,8 +1,6 @@
 <?PHP
 
-echo '<h1 style="color:#720b3d"> QUoTZee </h1>';
 
-echo '<br />';
 
 
 $servername = "localhost:3306";
@@ -22,30 +20,83 @@ if (mysqli_connect_errno())
 
 $name= $_POST['name'];
 
-echo' <h3 style="color:green"> Welcome to Quotzee. <br> You can post your quotes and read others here<br/><br/> <br /> <br />';
+//Title 
 
-echo "welcome ".$name;
 
-echo '<a href="post.php" > Post your new quote here </a> <br/>';
+echo'
+	<div class="container">
+
+  	<div class="jumbotron">
+	<h1 style="color:#720b3d"> QUoTZee </h1>
+	
+	 <h3 style="color:green"> Welcome to Quotzee. <br> You can post your quotes and read others here<br/><br/> ';
+
+//Bootstrap 
+echo '<head>
+  <title>QUoTZee</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>';
+echo '<body>';
+
+echo '<a href="post.php" class="btn btn-lg btn-primary"> Post your new quote here </a> <br />	<br/>';
+
 //Selecting datas from feed table
 $sql = "SELECT * FROM feed";
 
 $result = mysqli_query($conn,$sql)or die(mysqli_error());
 
-echo '<table>';
-echo '<tr><th>name</th><th>Post</th></tr>';
+// Table creation and headers
+echo '	
+	  <input class="form-control" id="myInput" type="text" placeholder="Search or filter" > ';
 
+echo '<table class="table table-hover table-striped">';
+echo '<thead><tr>
+	<th>Name </th>
+	
+	<th>Post</th>
+	</tr> 
+</thead>';
+
+
+
+//Fetching datas from table
 while($row = mysqli_fetch_array($result)) {
     $name = $row['name'];
     
     $post = $row['post'];
    
 
-    echo '<tr><td style="width: 200px;">'.$name.'</td><td style="width: 600px;">'.$post.'</td></tr>';
-} 
+    echo '	
+	    <tbody id="myTable">
+    <tr>	    
+	<td style="width: 200px;">'.$name.'</td>	
+	<td style="width: 600px;">'.$post.'</td>
+    </tr>';
 
-echo ' <br/> </table>';
+}
+
+
+echo ' <br/> <tbody> </table>';
+echo'
+
+<script>
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+</script>
+';
+
 mysqli_close($conn);
-echo '<a href="logout.php"> Logout </a>';
+echo '<a href="logout.php"> Logout </a>
+	</div> </div>';
 
 ?>
