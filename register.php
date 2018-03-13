eebd2d9d3fc876721879b492e12084d614baf6b1
Farooq khan <vsfarooqkhan@gmail.com>
2018-03-13 19:53:25 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,11 +1,23 @@
 <?php
 ?>
 <html>
-<body bgcolor="#d0d2e2">
+
+ <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+
+
+<body class="bg-info" bgcolor="#d0d2e2" >
 <form action="insert.php" name="input" method="post">
 
 
-	<h2 style="color:brown"> Registration </h2>
+ 
+	<h2 style="size:34;"> 
+	          <span class="glyphicon glyphicon-plus"></span>
+				Registration </h2>
 
     <p>
 	<label for="User name">User Name  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;:</label>
@@ -32,14 +44,15 @@
 </p>	    
 
 <p>
-    <input type="submit" value="Submit">
+    <input type="submit" value="Submit" class="btn btn-primary " >
 
 </p>
 
 <!-- Login page -->
-<a href="login.php"> Already an user?. Login here. </a>
+<a href="login.php"  class="text-white bg-dark"> Already an user?. Login here. </a>
 
 
 </form>
 </body>
 </html>
+
