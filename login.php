26d4dc5229b439b1bddb119cb9f1632e90b5e39b
Farooq khan <vsfarooqkhan@gmail.com>
2018-03-13 19:40:34 +0530

    Update login.php

--- login.php
+++ login.php
@@ -2,21 +2,40 @@
 ?>
 
 <html>
-<body bgcolor="#04e1f9">
+
+<head>
+  <title>QUoTZee</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
 
 
+<body  bgcolor="blue" class="bg-dark">
+<div class="container">
+<div class="jumbotron">
 <h1 style="color:#720b3d;font-family:chiller"> QUoTZee </h1>
+</div>
 
 <form action="loggin.php" action="homepage.php" name="login" method="post">
 
-User Name:<input type="text" name="name"/><br />
+<div class="col-xs-2">
+	<label for="usr">Name:</label>
+	<input type="text" name="name" class="form-control" /><br />
 
-Password &nbsp; :<input type="password" name="passwd"/><br />
+	<label for="usr">Password:</label>
+	<input type="password" name="passwd" class="form-control" /><br />
 
 
-<input type="submit" name="submit">
-<h4 style="color:#10198c"> Not a user yet??  <a href="register.php" > Click here to register </a>  </h4>
+	<input type="submit" name="submit" class="btn btn-primary">
+<br />
 
+<h4 style="color:#10198c"> Not a user yet ? <br/> <a href="register.php" class="btn btn-link btn-lg " > Click here to register </a>  </h4>
+<a href="quotzee.php"><span class="glyphicon glyphicon-home btn-md">Back to Home. </span></a>
+</div>
+</div>
 </form>
 
 </body>
